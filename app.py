615aae8e86706c2e012bdd8fe77856037e556e47
Roberto Oliveira <roberto.oliveira.ti@gmail.com>
2021-05-13 00:28:24 +0100

    Add login view

--- app.py
+++ app.py
@@ -61,6 +61,11 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
 @app.route("/profile")
 def profile():
     return render_template("profile.html")
