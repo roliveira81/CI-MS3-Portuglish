663013e521b6f82048699219384d686b714100b6
Roberto Oliveira <roberto.oliveira.ti@gmail.com>
2021-05-13 01:02:20 +0100

    Add profile view and signout funcionality

--- app.py
+++ app.py
@@ -54,7 +54,8 @@ def register():
         mongo.db.users.insert_one(register)
 
         # put the new user into 'session' cookie
-        session["user"] = request.form.get("email").lower()
+        session["user"] = request.form.get("name").lower()
+        session["email"] = request.form.get("email").lower()                
         flash("Registration Successful!", category='success')
         return redirect(url_for("profile", email=session["user"]))
 
@@ -72,11 +73,12 @@ def login():
             # ensure hashed password matches user input
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
-                        session["user"] = request.form.get("email").lower()
+                        session["user"] = existing_user["name"]
+                        session["email"] = existing_user["email"]                        
                         flash("Welcome, {}".format(
-                            request.form.get("email")), category='success')
+                             existing_user["name"]), category='success')
                         return redirect(url_for(
-                            "profile", email=session["user"]))
+                            "profile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password", category='danger')
@@ -90,9 +92,24 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/profile")
-def profile():
-    return render_template("profile.html")
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"email": session["email"]})["name"]
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been signed out", category='success')
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
