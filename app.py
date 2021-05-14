246868e4f309ed8d15362525d485f77297fa3ca2
Roberto Oliveira <roberto.oliveira.ti@gmail.com>
2021-05-14 03:06:57 +0100

    PEP8 validation review

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 
 # mongo obj flask app connection
-mongo = PyMongo(app, ssl=True,ssl_cert_reqs='CERT_NONE')
+mongo = PyMongo(app, ssl=True, ssl_cert_reqs='CERT_NONE')
 
 
 @app.route("/")
@@ -29,9 +29,10 @@ def index():
     """
     Main view. It loads all active posts for browse and search
     """
-    # fill the name, title and description dinamically to each page by page header on base template    
-    section = { "view": "Portuglish"  ,
-                "title": "The Survival Glossary for Brazilians Abroad"}    
+    # fill the name, title and description dinamically to each page
+    section = {
+        "view": "Portuglish",
+        "title": "The Survival Glossary for Brazilians Abroad"}
     # get only active posts
     posts = list(mongo.db.posts.find({"active": "1"}).sort('created', -1))
     return render_template("index.html", posts=posts, section=section)
@@ -40,15 +41,16 @@ def index():
 @app.route("/search", methods=["GET", "POST"])
 def search():
     """
-    The search feature on the main view between the published and active posts 
+    The search feature on the main view between the published and active posts
     """
-    section = { "view": "Portuglish"  ,
-                "title": "The Survival Glossary for Brazilians Abroad"}        
+    section = {
+        "view": "Portuglish",
+        "title": "The Survival Glossary for Brazilians Abroad"}
     query = request.form.get("query")
     if query == '':
         return redirect(url_for("index"))
-        
-    posts = list(mongo.db.posts.find({"$text": {"$search": query}}).sort('created', -1))
+    posts = list(mongo.db.posts.find(
+                {"$text": {"$search": query}}).sort('created', -1))
     return render_template("index.html", posts=posts, section=section)
 
 
@@ -57,8 +59,9 @@ def about():
     """
     Static view. It brings the project conception explanation.
     """
-    section = { "view": "About"  ,
-                "title": "The Survival Glossary for Brazilians Abroad"}    
+    section = {
+        "view": "About",
+        "title": "The Survival Glossary for Brazilians Abroad"}
     return render_template("about.html", section=section)
 
 
@@ -68,10 +71,11 @@ def create_post():
     The feature that provides the form to create new Posts
     """
     username = session["user"]
-    section = { "view": "New Post",
-                "title": f"Hi, {username}! We can't wait for your ideas"}  
+    section = {
+        "view": "New Post",
+        "title": f"Hi, {username}! We can't wait for your ideas"}
     if request.method == "POST":
-        active = "1" if request.form.get("active") == "on" else "0"        
+        active = "1" if request.form.get("active") == "on" else "0"
         post = {
             "category": request.form.get("category"),
             "title": request.form.get("title"),
@@ -87,18 +91,23 @@ def create_post():
         return redirect(url_for("index"))
 
     categories = mongo.db.categories.find().sort("name", 1)
-    return render_template("create_post.html", categories=categories, section=section, username=session["user"]) 
+    return render_template(
+                            "create_post.html",
+                            categories=categories,
+                            section=section,
+                            username=session["user"])
 
 
 @app.route("/edit_post/<_id>", methods=["GET", "POST"])
 def edit_post(_id):
     """
-    The feature that provides the form to update some post    
+    The feature that provides the form to update some post
     """
-    section = { "view": "Edit Post"  ,
-                "title": "The Survival Glossary for Brazilians Abroad"}        
+    section = {
+        "view": "Edit Post",
+        "title": "The Survival Glossary for Brazilians Abroad"}
     if request.method == "POST":
-        active = "1" if request.form.get("active") == "on" else "0"        
+        active = "1" if request.form.get("active") == "on" else "0"
         post = {
             "category": request.form.get("category"),
             "title": request.form.get("title"),
@@ -113,8 +122,13 @@ def edit_post(_id):
         flash("Task Successfully Updated", category='success')
 
     post = mongo.db.posts.find_one({"_id": ObjectId(_id)})
-    categories = mongo.db.categories.find().sort("name", 1)                
-    return render_template("edit_post.html", post=post, categories=categories, section=section, username=session["user"])
+    categories = mongo.db.categories.find().sort("name", 1)
+    return render_template(
+                            "edit_post.html",
+                            post=post,
+                            categories=categories,
+                            section=section,
+                            username=session["user"])
 
 
 @app.route("/delete_post/<_id>")
@@ -135,8 +149,9 @@ def like_post(_id):
     """
     post = (mongo.db.posts.find_one({"_id": ObjectId(_id)}))
     likes_updated = (int(post['like']) + 1)
-    mongo.db.posts.update({"_id": ObjectId(ObjectId(_id))}, {"$set": {"like": likes_updated}})   
-    return redirect(url_for("index"))    
+    mongo.db.posts.update({"_id": ObjectId(ObjectId(_id))},
+                          {"$set": {"like": likes_updated}})
+    return redirect(url_for("index"))
 
 
 @app.route("/dislike_post/<_id>")
@@ -144,11 +159,12 @@ def dislike_post(_id):
     """
     Feature to allow users to dislike posts, storing the negative
     votes in the DB
-    """    
+    """
     post = (mongo.db.posts.find_one({"_id": ObjectId(_id)}))
     dislikes_updated = (int(post['dislike']) + 1)
-    mongo.db.posts.update({"_id": ObjectId(ObjectId(_id))}, {"$set": {"dislike": dislikes_updated}})    
-    return redirect(url_for("index")) 
+    mongo.db.posts.update({"_id": ObjectId(ObjectId(_id))},
+                          {"$set": {"dislike": dislikes_updated}})
+    return redirect(url_for("index"))
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -156,15 +172,18 @@ def register():
     """
     Form to allow new users to become a collaborator by registering themselves
     """
-    section = { "view": "Be a collaborator"  ,
-                "title": "Help the Brazilian community to better express their ideas"}         
+    section = {
+        "view": "Be a collaborator",
+        "title": "Help the Brazilian community to better express their ideas"}
     if request.method == "POST":
         # check if username already exists in db
         existing_user = mongo.db.users.find_one(
             {"email": request.form.get("email").lower()})
 
         if existing_user:
-            flash("There is another user already registered under this e-Mail", category='danger')
+            flash(
+                "There is another user already registered under this e-Mail",
+                category='danger')
             return redirect(url_for("register"))
 
         register = {
@@ -176,7 +195,7 @@ def register():
 
         # put the new user into 'session' cookie
         session["user"] = request.form.get("name").lower()
-        session["email"] = request.form.get("email").lower()                
+        session["email"] = request.form.get("email").lower()
         flash("Registration Successful!", category='success')
         return redirect(url_for("profile", username=session["user"]))
 
@@ -188,8 +207,9 @@ def login():
     """
     Sign in feature, to allow collaborators access the posts manage section
     """
-    section = { "view": "Sign In"  ,
-                "title": "Help the Brazilian community to better express their ideas"}        
+    section = {
+        "view": "Sign In",
+        "title": "Help the Brazilian community to better express their ideas"}
     if request.method == "POST":
         # check if username exists in db
         existing_user = mongo.db.users.find_one(
@@ -200,7 +220,7 @@ def login():
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
                         session["user"] = existing_user["name"]
-                        session["email"] = existing_user["email"]                        
+                        session["email"] = existing_user["email"]
                         flash("Welcome, {}".format(
                              existing_user["name"]), category='success')
                         return redirect(url_for(
@@ -224,13 +244,18 @@ def profile(username):
     Feature to allow users to manage their posts, editing, deleting,
     publish and unpublish existent posts
     """
-    section = { "view": "Welcome",
-                "title": f"Hi, {username}! We can't wait for your ideas"}       
-    
+    section = {
+        "view": "Welcome",
+        "title": f"Hi, {username}! We can't wait for your ideas"}
     if session["user"]:
         # get only all colaborator posts (active/inactive)
-        posts = list(mongo.db.posts.find({"email_creator": session["email"]}).sort('created', -1))
-        return render_template("profile.html", posts=posts, username=session["user"], section=section)
+        posts = list(mongo.db.posts.find(
+                    {"email_creator": session["email"]}).sort('created', -1))
+        return render_template(
+                                "profile.html",
+                                posts=posts,
+                                username=session["user"],
+                                section=section)
 
     return redirect(url_for("login"))
 
@@ -248,5 +273,4 @@ def logout():
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=True)
+            port=int(os.environ.get("PORT")), debug=True)
